388d0b76ca2900b4c205ba18b8e4bf5ab9f952d9
v-amna <amnamampad@gmail.com>
2026-06-16 15:05:14 +0200

    add books/templates for view books

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
 
 # Create your views here.
+def book_list(request):
+   
+    return render(request, 'books/book_list.html')
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'books/book_detail.html', {'book': book})
