c39ca9689af0626cd6c3b5bb3ff39ac7d4424062
v-amna <amnamampad@gmail.com>
2026-06-18 13:06:23 +0200

    Impliment book search,category filtering and catalogue styling

--- books/views.py
+++ books/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from books.models import Book,Category
 
-from books.models import Book
 
 
 # Create your views here.
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'books/book_list.html', {'books': books})
+    categories = Category.objects.all()
+    query = request.GET.get('q')
+    category=request.GET.get('category')
+
+    if query:
+        books = books.filter(
+            Q(title__icontains=query) |
+            Q(author__icontains=query) |
+            Q(isbn__icontains=query)
+        )
+    if category:
+        books = books.filter(category__name=category    )
+    
+    return render(request, 'books/book_list.html', {'books': books, 'categories': categories})
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
