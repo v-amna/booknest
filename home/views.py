ee89f834a50b6bbf150ebef2fec96168a1113a12
v-amna <amnamampad@gmail.com>
2026-06-18 17:06:16 +0200

    Display featured books from database on homepage

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from books.models import Book
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    featured_books = Book.objects.all()[:3]
+
+    context = {
+        'featured_books': featured_books,
+    }
+    return render(request, 'home/index.html', context)
