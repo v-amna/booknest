2a43bc459275f2a5e6b68aea1b76bde418421347
v-amna <amnamampad@gmail.com>
2026-06-24 12:25:35 +0200

    Create order and order line item models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from django.contrib.auth.models import User
+from profiles.models import UserProfile
+from books.models import Book
 
 # Create your models here.
+
+
+
+class Order(models.Model):
+
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    full_name = models.CharField(
+        max_length=50
+    )
+
+    email = models.EmailField(
+        max_length=254
+    )
+
+    phone_number = models.CharField(
+        max_length=20
+    )
+
+    street_address1 = models.CharField(
+        max_length=80
+    )
+
+    street_address2 = models.CharField(
+        max_length=80,
+        blank=True
+    )
+
+    town_or_city = models.CharField(
+        max_length=40
+    )
+
+    postcode = models.CharField(
+        max_length=20,
+        blank=True
+    )
+
+    country = models.CharField(
+        max_length=40
+    )
+
+    date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    order_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+
+class OrderLineItem(models.Model):
+
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='lineitems'
+    )
+
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=1
+    )
+
+    lineitem_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        editable=False
+    )
+
+    def save(self, *args, **kwargs):
+
+        self.lineitem_total = (
+            self.book.price * self.quantity
+        )
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return (
+            f"{self.book.title} "
+            f"on Order {self.order.id}"
+        )
