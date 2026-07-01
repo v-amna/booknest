3788157884f6b2f3d95b8e73b023e036ac5fcb01
v-amna <amnamampad@gmail.com>
2026-07-01 15:06:53 +0200

    Product Admin- image field fixing

--- books/forms.py
+++ books/forms.py
@@ -1,8 +1,15 @@
+from .widgets import CustomClearableFileInput
 from django import forms
 from .models import Book
 
 
 class BookForm(forms.ModelForm):
+
+    cover_image = forms.ImageField(
+        label="Image",
+        required=False,
+        widget=CustomClearableFileInput()
+    )
 
     class Meta:
         model = Book
---
+++ books/widgets.py
@@ -0,0 +1,5 @@
+from django.forms.widgets import ClearableFileInput
+
+
+class CustomClearableFileInput(ClearableFileInput):
+    template_name = "books/custom_widget_templates/custom_clearable_file_input.html"
